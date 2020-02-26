793011a2d978a6d893e921943430907d638e8492
ThomasLouch <s2element909@gmail.com>
2020-02-26 08:59:44 +0000

    Update audioUtilities.js
    lol

--- src/js/util/audioUtilities.js
+++ src/js/util/audioUtilities.js
@@ -4,7 +4,7 @@ const sound_map = {
     new Audio('/static/audio/power-logo.mp3')
   ],
   cum: [
-    new Audio('/static/audio/cum-1.mp3')
+    new Audio('/static/audio/cum-1.mp3'),
     new Audio('/static/audio/cum-2.mp3')
   ],
   nope: [
